bfc5e708fff2f2b2d280b3a9f864ae3941c847b9
Ketan Sonawane <sonawaneketan1819@gmail.com>
2026-02-11 20:03:48 +0530

    Added all payload extraction methods

--- UberProject-AuthService/src/main/java/com/example/UberProject_AuthService/service/JWTService.java
+++ UberProject-AuthService/src/main/java/com/example/UberProject_AuthService/service/JWTService.java
@@ -1,5 +1,6 @@
 package com.example.UberProject_AuthService.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTService implements CommandLineRunner {
@@ -24,7 +27,6 @@ public class JWTService implements CommandLineRunner {
     // this method creates a brand new JWT token
     public String createToken(Map<String, Object> payload, String username) {
         Date now = new Date();
-        SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
         Date expiryDate = new Date(now.getTime() + expiry * 1000L);
 
         return Jwts.builder()
@@ -32,18 +34,63 @@ public class JWTService implements CommandLineRunner {
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .setSubject(username)
-                .signWith(key)
+                .signWith(getSignKey())
                 .compact();
     }
 
-    @Override
-    public void run(String... args) throws Exception
-    {
-        Map<String,Object> mp = new HashMap<>();
-        mp.put("email","a@bgmail");
-        mp.put("phoneNumber","6854254135");
-        String result = createToken(mp,"Ketan");
+    // converts the encoded token string to actual payload
+    private <T> T extractPayload(String token, Function<Claims, T> resolverFunction) {
+        final Claims claims = Jwts
+                .parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
 
-        System.out.println("Generated token is : "+result);
+        return resolverFunction.apply(claims);
+    }
+
+    // extract expiration date
+    private Date extractExpiration(String token) {
+        return extractPayload(token, Claims::getExpiration);
+    }
+
+    // extract email from token
+    private String extractEmail(String token) {
+        return extractPayload(token, claims -> claims.get("email", String.class));
+    }
+
+    // check if token is expired
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    // create signing key
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // validate token
+    private Boolean validateToken(String token, String email) {
+        final String userEmailFetchedFromToken = extractEmail(token);
+        return userEmailFetchedFromToken.equals(email) && !isTokenExpired(token);
+    }
+
+    private String extractPhoneNumber(String token)
+    {
+        return extractPayload(token, claims -> claims.get("phoneNumber", String.class));
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, Object> mp = new HashMap<>();
+        mp.put("email", "a@bgmail");
+        mp.put("phoneNumber", "6854254135");
+        String result = createToken(mp, "Ketan");
+
+
+        System.out.println("Generated token is : " + result);
+        System.out.println(extractPhoneNumber(result));
     }
 }
